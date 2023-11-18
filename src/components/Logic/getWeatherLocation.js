1b10f1691a7ec8dc36da08cef63f3144091ab8f1
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-11-18 03:57:18 -0500

    Add new folder components which holds the dom and logic

---
+++ src/components/Logic/getWeatherLocation.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line consistent-return
+async function getCurrentWeatherLocationData(location) {
+  try {
+    const weatherResponse = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=bcbc49485145475c855175911231711&q=${location}&days=3`,
+      { mode: "cors" }
+    );
+
+    if (!weatherResponse.ok) {
+      throw new Error("FAILED STATUS");
+    }
+
+    const weatherData = await weatherResponse.json();
+    console.log(weatherData);
+
+    return weatherData;
+  } catch (error) {
+    console.error(`ERROR HAS BEEN DETECTED: ${error}`);
+  }
+}
+
+export default getCurrentWeatherLocationData;
